75b45932bd0a0091c039e3754df549bff3c7fe3d
sukalov <matvey10@gmail.com>
2023-06-01 16:01:39 -0600

    bun

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const fs = require('fs')
 const countries = require ('./restcountries.json');
 const cors = require('cors');
+const { time, timeStamp } = require('console');
 
 const app = express()
 const PORT = process.env.PORT || 3000
@@ -9,9 +10,10 @@ const PORT = process.env.PORT || 3000
 app.use(cors());
 
 app.get('/', (req, res) => {
+  console.log('GET', new Date())
   res.json(countries)
 });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`)
-})
+});
